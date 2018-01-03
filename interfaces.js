b0158a752dd75efabcfdc8bf3d682db962b36526
Tom Anderson <portlester.tom@gmail.com>
2018-01-03 14:41:24 +1100

    Function to make buttons and class to select button

--- interfaces.js
+++ interfaces.js
@@ -1,6 +1,29 @@
 // Global variable for the parent div
 let domInterface = document.getElementById("interface");
 
+// Makes and returns a button
+function makeButton(id, classList, innerHTML) {
+    // New button
+    let newButton = document.createElement("button");
+
+    // Do the attributes
+    if (id) {
+        newButton.id = id;
+    }
+
+    if (innerHTML) {
+        newButton.innerHTML = innerHTML;
+    }
+
+    if (classList) {
+        for (newClass of classList) {
+            newButton.classList.add(newClass);
+        }
+    }
+
+    return newButton;
+}
+
 function mainMenu() {
     // Container for the main menu
     let domMainMenu = document.createElement("div");
@@ -12,14 +35,14 @@ function mainMenu() {
     domMainMenuHeading.id = "mainMenuHeading";
     domMainMenu.appendChild(domMainMenuHeading);
 
-    // Buttons for menu
-    for (i=0; i < 3; i++) {
-        let domNewButton = document.createElement("button");
+    let newButton = makeButton(undefined, ["buttonSelected"], "Button #1");
+    domMainMenu.appendChild(newButton);
 
-        domNewButton.innerHTML = "Button #" + i;
+    let newButton2 = makeButton(undefined, undefined, "Button #2");
+    domMainMenu.appendChild(newButton2);
 
-        domMainMenu.appendChild(domNewButton);
-    }
+    let newButton3 = makeButton(undefined, undefined, "Button #3");
+    domMainMenu.appendChild(newButton3);
 
     domInterface.appendChild(domMainMenu);
 }
