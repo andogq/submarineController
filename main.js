972d5da614f083fe7ec223eb4ff4d134c1f182c3
Tom Anderson <portlester.tom@gmail.com>
2018-01-04 22:07:58 +1100

    Prevent spamming buttons

--- main.js
+++ main.js
@@ -22,6 +22,36 @@ let joystickEvent = {
     "axesMiniUpDown": function() {return;}
 };
 
+// Holds the last time of all the buttons pressed so that there isn't multiple presses for holding down too long
+let lastTimePressed = {
+    "trigger": 0,
+    "thumb": 0,
+    "bottomLeftButton": 0,
+    "bottomRightButton": 0,
+    "topLeftButton": 0,
+    "topRightButton": 0,
+    "topLeftPadButton": 0,
+    "topRightPadButton": 0,
+    "middleLeftPadButton": 0,
+    "middleRightPadButton": 0,
+    "bottomLeftPadButton": 0,
+    "bottomRightPadButton": 0,
+    "axesMiniLeftRight": 0,
+    "axesMiniUpDown": 0
+}
+
+// Prevents the button (mini axes included) from spamming by being held down
+function buttonHeld(button) {
+    // If they are 500 milliseconds apart
+    if ((Date.now() - lastTimePressed[button]) > 500) {
+        // Change to the new time
+        lastTimePressed[button] = Date.now();
+        return true;
+    } else {
+        return false;
+    }
+}
+
 function mainLoop() {
     // Save the joystick state in a variable
     let joystick = navigator.getGamepads()[joystickIndex];
@@ -30,61 +60,85 @@ function mainLoop() {
     if (joystick.buttons[0].pressed) {
         // console.log("Trigger pressed");
         joystickEvent.trigger(joystick);
+    } else { // Prevents spamming
+        lastTimePressed["trigger"] = 0;
     }
 
     if (joystick.buttons[1].pressed) {
         // console.log("Thumb button pressed");
         joystickEvent.thumb(joystick);
+    } else {
+        lastTimePressed["thumb"] = 0;
     }
 
     if (joystick.buttons[2].pressed) {
         // console.log("Bottom left button pressed");
         joystickEvent.bottomLeftButton(joystick);
+    } else {
+        lastTimePressed["bottomLeftButton"] = 0;
     }
 
     if (joystick.buttons[3].pressed) {
         // console.log("Bottom right button pressed");
         joystickEvent.bottomRightButton(joystick);
+    } else {
+        lastTimePressed["bottomRightButton"] = 0;
     }
 
     if (joystick.buttons[4].pressed) {
         // console.log("Top left button pressed");
         joystickEvent.topLeftButton(joystick);
+    } else {
+        lastTimePressed["topLeftButton"] = 0;
     }
 
     if (joystick.buttons[5].pressed) {
         // console.log("Top right button pressed");
         joystickEvent.topRightButton(joystick);
+    } else {
+        lastTimePressed["topRightButton"] = 0;
     }
 
     if (joystick.buttons[6].pressed) {
         // console.log("Button pad top left pressed");
         joystickEvent.topLeftPadButton(joystick);
+    } else {
+        lastTimePressed["topLeftPadButton"] = 0;
     }
 
     if (joystick.buttons[7].pressed) {
         // console.log("Button pad top right pressed");
         joystickEvent.topRightPadButton(joystick);
+    } else {
+        lastTimePressed["topRightPadButton"] = 0;
     }
 
     if (joystick.buttons[8].pressed) {
         // console.log("Button pad middle left pressed");
         joystickEvent.middleLeftPadButton(joystick);
+    } else {
+        lastTimePressed["middleLeftPadButton"] = 0;
     }
 
     if (joystick.buttons[9].pressed) {
         // console.log("Button pad middle right pressed");
         joystickEvent.middleRightPadButton(joystick);
+    } else {
+        lastTimePressed["middleRightPadButton"] = 0;
     }
 
     if (joystick.buttons[10].pressed) {
         // console.log("Button pad bottom left pressed");
         joystickEvent.bottomLeftPadButton(joystick);
+    } else {
+        lastTimePressed["bottomLeftPadButton"] = 0;
     }
 
     if (joystick.buttons[11].pressed) {
         // console.log("Button pad bottom right pressed");
         joystickEvent.bottomRightPadButton(joystick);
+    } else {
+        lastTimePressed["bottomRightPadButton"] = 0;
     }
 
     // Check the axes
@@ -108,11 +162,15 @@ function mainLoop() {
     if (joystick.axes[4] != 0) {
         joystickEvent.axesMiniLeftRight(joystick);
         //console.log("Mini left right axes: " + joystick.axes[4]);
+    } else {
+        lastTimePressed["axesMiniLeftRight"] = 0;
     }
 
     if (joystick.axes[5] != 0) {
         joystickEvent.axesMiniUpDown(joystick);
         // console.log("Mini up down axes: " + joystick.axes[5]);
+    } else {
+        lastTimePressed["axesMiniUpDown"] = 0;
     }
 
     requestAnimationFrame(mainLoop);
