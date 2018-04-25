2a81bf393480b3df91a8ff7e58662956b805aeb8
Tom Anderson <portlester.tom@gmail.com>
2018-04-25 16:36:46 +1000

    Added keyboard support

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -14,5 +14,89 @@ server.onmessage = function(event) {
     websocketMessage(event);
 }
 
+// Empty joystick object to emulate
+const emptyJoystick = {
+    axes: [
+        0,
+        0,
+        0,
+        0,
+        0,
+        0
+    ],
+    buttons: [
+        {
+            pressed: false
+        },
+        {
+            pressed: false
+        },
+        {
+            pressed: false
+        },
+        {
+            pressed: false
+        },
+        {
+            pressed: false
+        },
+        {
+            pressed: false
+        },
+        {
+            pressed: false
+        },
+        {
+            pressed: false
+        },
+        {
+            pressed: false
+        },
+        {
+            pressed: false
+        },
+        {
+            pressed: false
+        },
+        {
+            pressed: false
+        }
+    ]
+}
+
+// Event listeners for keyboard (to emulate joystick)
+document.addEventListener("keydown", (e) => {
+    let tmpJoystick = emptyJoystick;
+    switch (e.key) {
+        case "ArrowUp":
+            tmpJoystick.axes[5] = -1;
+            joystickEvent.axesMiniUpDown(tmpJoystick);
+            break;
+        case "ArrowDown":
+            tmpJoystick.axes[5] = 1;
+            joystickEvent.axesMiniUpDown(tmpJoystick);
+            break;
+        case "ArrowLeft":
+            break;
+        case "ArrowRight":
+            break;
+        case "w":
+            break;
+        case "s":
+            break;
+        case "a":
+            break;
+        case "d":
+            break;
+        case "Enter":
+            joystickEvent.trigger();
+            break;
+        case "Escape":
+            break;
+        default:
+            break;
+    }
+})
+
 // Make the main menu
 mainMenu();
